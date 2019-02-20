a01fb0c2d1c1c92a4ff456a9b685d945b5b83f9e
zaka29 <zaka29@gmail.com>
2019-02-20 21:52:33 +1000

    Project files, soccer players collection

--- SoccerLeague.playground/Contents.swift
+++ SoccerLeague.playground/Contents.swift
@@ -1,4 +1,21 @@
 //: Playground - noun: a place where people can play
-
-
-var str = "Hello, playground"
+let players: [[String: String]] = [
+    ["name" : "Joe Smith", "Height" : "42", "experience" : "yes", "guardianName" : "Jim and Jan Smith"],
+    ["name" : "Jill Tanner", "Height" : "36", "experience" : "yes", "guardianName" : "Clara Tanner"],
+    ["name" : "Bill Bon", "Height" : "43", "experience" : "yes", "guardianName" : "Sara and Jenny Bon"],
+    ["name" : "Eva Gordon", "Height" : "45", "experience" : "no", "guardianName" : "Wendy and Mike Gordon"],
+    ["name" : "Matt Gill", "Height" : "40", "experience" : "no", "guardianName" : "Charles and Sylvia Gill"],
+    ["name" : "Kimmy Stein", "Height" : "41", "experience" : "no", "guardianName" : "Bill and Hillary Stein"],
+    ["name" : "Sammy Adams", "Height" : "45", "experience" : "no", "guardianName" : "Jeff Adams"],
+    ["name" : "Karl Saygan", "Height" : "42", "experience" : "yes", "guardianName" : "Heather Bledsoe"],
+    ["name" : "Suzane Greenberg", "Height" : "44", "experience" : "yes", "guardianName" : "Henrietta Dumas"],
+    ["name" : "Suzane Greenberg", "Height" : "41", "experience" : "no", "guardianName" : "Gala Dali"],
+    ["name" : "Joe Kavalier", "Height" : "39", "experience" : "no", "guardianName" : "Sam and Elaine Kavalier"],
+    ["name" : "Ben Finkelstein", "Height" : "44", "experience" : "no", "guardianName" : "Aaron and Jill Finkelstein"],
+    ["name" : "Diego Soto", "Height" : "41", "experience" : "yes", "guardianName" : "Robin and Sarika Soto"],
+    ["name" : "Chloe Alaska", "Height" : "47", "experience" : "no", "guardianName" : "David and Jamie Alaska"],
+    ["name" : "Arnold Willis", "Height" : "43", "experience" : "no", "guardianName" : "Claire Willis"],
+    ["name" : "Phillip Helm", "Height" : "44", "experience" : "yes", "guardianName" : "Thomas Helm and Eva Jones"],
+    ["name" : "Les Clay", "Height" : "42", "experience" : "yes", "guardianName" : "Wynonna Brown"],
+    ["name" : "Herschel Krustofski", "Height" : "45", "experience" : "yes", "guardianName" : "Hyman and Rachel Krustofski"],
+]
